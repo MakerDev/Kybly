9ed9faf21ecea9520599d769099cc829e64e2b66
유진 신 <raki2001@naver.com>
2020-07-28 14:11:18 +0900

    FlatButttonStyle applied

---
+++ AutoHotkeyRemaster.Services/IJsonSavefileManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHotkeyRemaster.Services
+{
+    public interface IJsonSavefileManager
+    {
+        void Save<T>(T instance, string filename, bool appendExtension = true) where T : class;
+        T Load<T>(string filename, bool appendExtenstion = true) where T : class;
+        bool DeleteIfExists(string filename, bool appendExtenstion = true);
+    }
+}
---
+++ AutoHotkeyRemaster.UI/Helpers/JsonSaveFileManager.cs
@@ -0,0 +1,71 @@
+﻿using AutoHotkeyRemaster.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace AutoHotkeyRemaster.WPF.Helpers
+{
+    public class JsonSaveFileManager : IJsonSavefileManager
+    {
+        //TODO : need to change in UWP. Should extract interface and use DI?
+        public static string SavefileFolder { get; private set; }
+            = Path.Combine(Environment.CurrentDirectory, "savefiles");
+
+        public JsonSaveFileManager()
+        {
+            if (!Directory.Exists(SavefileFolder))
+                Directory.CreateDirectory(SavefileFolder);
+        }
+
+        public bool DeleteIfExists(string filename, bool appendExtenstion = true)
+        {
+            string path = Path.Combine(SavefileFolder, filename);
+
+            if (appendExtenstion)
+                path += ".json";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Save<T>(T instance, string filename, bool appendExtenstion = true) where T : class
+        {
+            string path = Path.Combine(SavefileFolder, filename);
+
+            if (appendExtenstion)
+                path += ".json";
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            string jsonString = JsonSerializer.Serialize(instance, options);
+            File.WriteAllText(path, jsonString);
+        }
+
+        public T Load<T>(string filename, bool appendExtenstion = true) where T : class
+        {
+            string path = Path.Combine(SavefileFolder, filename);
+
+            if (appendExtenstion)
+                path += ".json";
+
+            if (!File.Exists(path))
+                return null;
+
+            string jsonString = File.ReadAllText(path);
+            T instance = JsonSerializer.Deserialize<T>(jsonString);
+
+            return instance;
+        }
+
+    }
+}
