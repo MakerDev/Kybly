89c8567f6a97abbf3ec5a7a69c90a0ddfaa1ce60
유진 신 <raki2001@o365.yonsei.ac.kr>
2020-07-29 19:31:34 +0900

    Changed if a keycode represents kind of 'Oem~', it will be translated into a proper char.

--- AutoHotkeyRemaster.UI/Converters/KeycodeToStringConverter.cs
+++ AutoHotkeyRemaster.UI/Converters/KeycodeToStringConverter.cs
@@ -2,11 +2,14 @@
 using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace AutoHotkeyRemaster.WPF.Converters
 {
     public class KeycodeToStringConverter : IValueConverter
     {
+        private KeyConverter _converter = new KeyConverter();
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             int keycode = (int)value;
@@ -15,7 +18,6 @@ namespace AutoHotkeyRemaster.WPF.Converters
             if (keycode == 1) { return "LButton"; }
             if (keycode == 2) { return "RButton"; }
             if (keycode == 4) { return "MButton"; }
-
 
             return VirtualKeycodeToWpfKeyConverter.ConverFrom((int)value);
         }
--- AutoHotkeyRemaster.UI/Helpers/VirtualKeycodeToWpfKeyConverter.cs
+++ AutoHotkeyRemaster.UI/Helpers/VirtualKeycodeToWpfKeyConverter.cs
@@ -1,24 +1,86 @@
 ﻿using AutoHotkeyRemaster.Models.Helpers;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Input;
 
 namespace AutoHotkeyRemaster.WPF.Helpers
 {
     public class VirtualKeycodeToWpfKeyConverter : IVirtualKeycodeToStringConverter
     {
+        public enum MapType : uint
+        {
+            MAPVK_VK_TO_VSC = 0x0,
+            MAPVK_VSC_TO_VK = 0x1,
+            MAPVK_VK_TO_CHAR = 0x2,
+            MAPVK_VSC_TO_VK_EX = 0x3,
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int ToUnicode(
+            uint wVirtKey,
+            uint wScanCode,
+            byte[] lpKeyState,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)]
+            StringBuilder pwszBuff,
+            int cchBuff,
+            uint wFlags);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        public static extern uint MapVirtualKey(uint uCode, MapType uMapType);
+
+        public static char GetCharFromKey(int vkCode)
+        {
+            char ch = ' ';
+
+            byte[] keyboardState = new byte[256];
+            GetKeyboardState(keyboardState);
+
+            uint scanCode = MapVirtualKey((uint)vkCode, MapType.MAPVK_VK_TO_VSC);
+            StringBuilder stringBuilder = new StringBuilder(2);
+
+            int result = ToUnicode((uint)vkCode, scanCode, keyboardState, stringBuilder, stringBuilder.Capacity, 0);
+            switch (result)
+            {
+                case -1:
+                    break;
+                case 0:
+                    break;
+                case 1:
+                    {
+                        ch = stringBuilder[0];
+                        break;
+                    }
+                default:
+                    {
+                        ch = stringBuilder[0];
+                        break;
+                    }
+            }
+
+            return ch;
+        }
+
         public static string ConverFrom(int keycode)
         {
             if (keycode == -1)
                 return "";
 
-            return KeyInterop.KeyFromVirtualKey(keycode).ToString();
+            string result = KeyInterop.KeyFromVirtualKey(keycode).ToString();
+
+            if(result.StartsWith("Oem"))
+            {
+                result = GetCharFromKey(keycode).ToString();
+            }
+
+            return result;
         }
 
         public string Convert(int keycode)
         {
-            if (keycode == -1)
-                return "";
-
-            return KeyInterop.KeyFromVirtualKey(keycode).ToString();
+            return ConverFrom(keycode);
         }
     }
 }
